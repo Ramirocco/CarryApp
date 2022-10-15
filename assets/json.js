3e3769159838576f4ed2414e05bcf5e49d017667
Ramirocco <105560507+Ramirocco@users.noreply.github.com>
2022-10-15 17:52:33 -0300

    termine fetch y carga de tarjetas

---
+++ assets/json.js
@@ -0,0 +1,63 @@
+function obtenerViajes() {
+    const archivo = '../assets/index.json';
+
+    fetch(archivo)
+        .then(resultado => resultado.json())
+        .then(datos => {
+            console.log(datos.viajesEnJSON);
+            const { viajesEnJSON } = datos;
+            console.log(viajesEnJSON);
+
+            viajesEnJSON.forEach(viaje => {
+                console.log(viaje);
+                console.log (viaje.diaSalida);
+
+
+
+
+
+                //ESCRITURA DE CODIGO VIAJE.ALGO
+
+
+                //Mensajes que cambian los truty
+                viaje.fumar == "true" ?   viaje.fumar = "usted podria fumar en el vehiculo" : viaje.fumar = "usted NO podria fumar en el vehiculo" ;
+                //mascota
+                viaje.mascotas == "true" ? viaje.mascotas = "usted podria llevar animales" : viaje.mascotas = "usted NO podria llevar animales" ;
+                //maleta
+                viaje.maletas == "true" ?  viaje.maletas = "usted podria llevar maleta" : viaje.maletas = "usted NO podria llevar maleta";
+                //peajes
+                viaje.peajes == "true" ?  viaje.peajes = "Los precios incluyen los peajes y gastos " : viaje.peajes = "Los precios NO incluyen los peajes y gastos " ;               
+                
+                let viajesDeJson = `<section ; " class="CardsViaje" style="padding: 5px;">
+                <div class="card" style="width: 100%;  padding: 5px;">
+                    <img src="./assets/img/images.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 id="tituloTarjetaCarry" class="card-title"><b>${viaje.origen} HASTA ${viaje.destino} </b></h5>
+                        <p id="mensajeCarry" class="card-text"><b>Mensaje del Carry:</b> ${viaje.mensaje}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li id="diaSalidaCarry" class="list-group-item"><b>El día de salida es el</b> ${viaje.diaSalida}</li>
+                        <li id="horaLlegadaCarry" class="list-group-item"><b>El carry llegará a destino a las </b>${viaje.horaLlegada}</li>
+                        <li id="idUnicoCarry" class="list-group-item"> <b>El ID es </b>${viaje.idUnico}</li>
+                        <li id="fumarCarry" class="list-group-item">${viaje.fumar}</li>
+                        <li id="mascotaCarry" class="list-group-item">${viaje.mascotas}</li>
+                        <li id="maletaCarry" class="list-group-item">${viaje.maletas}</li>
+                        <li id="peajeCarry" class="list-group-item">${viaje.peajes}</li>
+                        <li id="precioCarry" class="list-group-item">${viaje.precio}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="reservarAsiento">Contactar al Carry</a>
+                    </div>
+                </div>
+            </section>`;
+
+            let tarjetasDeJson = document.getElementById ("tarjetasJson");
+            function escribirTarjeta() { tarjetasDeJson.innerHTML += viajesDeJson };
+            //funcion SI con funcion de escribir, sino (sercarry.html) consolea eso. 
+            tarjetasDeJson != null ? escribirTarjeta() : console.log("No se encuentra el id");
+
+            });
+        })
+
+};
+obtenerViajes();
--- assets/ok.js
+++ assets/ok.js
@@ -8,6 +8,7 @@ let tarjetasEnHTML = document.getElementById("tarjetear");
 let infoFormulario;
 let nuevoViaje;
 let objetoViaje;
+let arraysViajes = [];
 
 //ESTA VARIABLE ES EL CONTENIDO DE LA TARJETA, QUE NO PUEDO HACERLA GLOBAL A TODOS LOS HTML
 let viajeEnTarjetaIndex;
@@ -51,47 +52,48 @@ class Viaje {
 }
 
 //FUNCION SI, SI HAY FORMULARIO CREA LA TARJETA A IMPRIMIR
-formularioCarry != null?
-//escucha evento Submit
-formularioCarry.addEventListener("submit", (e) => {
-    e.preventDefault();
-    infoFormulario = e.target.children;
-    //variables del form
-    let provinciaOrigenSelec = document.getElementById("provinciaOrigen").value;
-    let provinciaDestinoSelec = document.getElementById("provinciaDestino").value;
-    let horaSalidaSelec = document.getElementById("horaSalida").value;
-    let horaLlegadaSelec = document.getElementById("horaLlegada").value;
-    let diaSalidaSelec = document.getElementById("diaSalida").value;
-    let mensajeSelec = document.getElementById("mensajeAPasajero").value;
-    let fumarSelec = document.getElementById("fumar").checked;
-    let mascotasSelec = document.getElementById("mascota").checked;
-    let maletasSelec = document.getElementById("maleta").checked;
-    let peajesSelec = document.getElementById("peajes").checked;
-    let precioSelec = document.getElementById("precio").value;
-    let idSelec = Math.ceil(Math.random() * 10000000 + 10000);
+formularioCarry != null ?
+    //escucha evento Submit
+    formularioCarry.addEventListener("submit", (e) => {
+        e.preventDefault();
+        infoFormulario = e.target.children;
+        //variables del form
+        let provinciaOrigenSelec = document.getElementById("provinciaOrigen").value;
+        let provinciaDestinoSelec = document.getElementById("provinciaDestino").value;
+        let horaSalidaSelec = document.getElementById("horaSalida").value;
+        let horaLlegadaSelec = document.getElementById("horaLlegada").value;
+        let diaSalidaSelec = document.getElementById("diaSalida").value;
+        let mensajeSelec = document.getElementById("mensajeAPasajero").value;
+        let fumarSelec = document.getElementById("fumar").checked;
+        let mascotasSelec = document.getElementById("mascota").checked;
+        let maletasSelec = document.getElementById("maleta").checked;
+        let peajesSelec = document.getElementById("peajes").checked;
+        let precioSelec = document.getElementById("precio").value;
+        let idSelec = Math.ceil(Math.random() * 10000000 + 10000);
 
-    //objeto a subir al Local
-    nuevoViaje = {
-        idUnico: idSelec,
-        origen: provinciaOrigenSelec,
-        destino: provinciaDestinoSelec,
-        diaSalida: diaSalidaSelec,
-        horaLlegada: diaSalidaSelec,
-        horaLlegada: horaSalidaSelec,
-        mensaje: horaLlegadaSelec,
-        fumar: fumarSelec,
-        mascotas: mascotasSelec,
-        maletas: maletasSelec,
-        peajes: peajesSelec,
-        precio: precioSelec,
-    }
-    //subida de objeto
-    localStorage.setItem(`viaje ${idSelec}`, JSON.stringify(nuevoViaje));
+        //objeto a subir al Local
+        nuevoViaje = {
+            idUnico: idSelec,
+            origen: provinciaOrigenSelec,
+            destino: provinciaDestinoSelec,
+            diaSalida: diaSalidaSelec,
+            horaLlegada: diaSalidaSelec,
+            horaLlegada: horaSalidaSelec,
+            mensaje: horaLlegadaSelec,
+            fumar: fumarSelec,
+            mascotas: mascotasSelec,
+            maletas: maletasSelec,
+            peajes: peajesSelec,
+            precio: precioSelec,
+        }
+        //subida de objeto
+        localStorage.setItem(`viaje ${idSelec}`, JSON.stringify(nuevoViaje));
 
-    //ALERT PARA CONFIRMAR LA SUBIDA 
-    Swal.fire({
-        title: 'Casi listos, Carry',
-        html: `<h3>Si llenaste todo bien dale al botón Azul.</h3>
+
+        //ALERT PARA CONFIRMAR LA SUBIDA 
+        Swal.fire({
+            title: 'Casi listos, Carry',
+            html: `<h3>Si llenaste todo bien dale al botón Azul.</h3>
         <p>Origen: ${provinciaOrigenSelec}, Dia Salida: ${diaSalidaSelec}, Hora Salid: ${horaSalidaSelec} </p>
         <p>Destino:${provinciaDestinoSelec},Hora Llegada:${horaLlegadaSelec} </p>
         <p>Mensaje: ${mensajeSelec} </p>
@@ -101,20 +103,20 @@ formularioCarry.addEventListener("submit", (e) => {
         <p>Peajes:${peajesSelec} </p>
         <p>Precio:${precioSelec} </p>
         <p>ID ASIGNADO: ${idSelec}</p>`,
-        icon: 'info',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'entendido Bro',
-        //  SI CONFIRMA EL RESULTADO ME CREA LA ETIQUETA PRA IMPRIMIR
-    }).then((result) => {
-        if (result.isConfirmed) {
-            //Obtiene datos del Local
-            let cargados = JSON.parse(localStorage.getItem(`viaje ${idSelec}`));
-            //Crea por constructor la tarjeta
-            objetoViaje = new Viaje (cargados);
-            console.log (objetoViaje);
-            // VARIABLE PARA HACER GLOBAL (HTML que escribe}
-            viajeEnTarjetaIndex = `<section  class="CardsViaje">
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'entendido Bro',
+            //  SI CONFIRMA EL RESULTADO ME CREA LA ETIQUETA PRA IMPRIMIR
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //Obtiene datos del Local
+                let cargados = JSON.parse(localStorage.getItem(`viaje ${idSelec}`));
+                //Crea por constructor la tarjeta
+                objetoViaje = new Viaje(cargados);
+                console.log(objetoViaje);
+                // VARIABLE PARA HACER GLOBAL (HTML que escribe}
+                viajeEnTarjetaIndex = `<section  class="CardsViaje">
                 <div class="card" style="width: 18rem">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
@@ -138,13 +140,52 @@ formularioCarry.addEventListener("submit", (e) => {
                     </div>
                 </div>
             </section>`;
-            console.log (viajeEnTarjetaIndex);
-            
-        }
-        //Si no hay formulario (index.html) Consolea esto
-        });}):console.log ("No se encuentra FormularioCarry");
-        //FUNCION SI, SI EXISTE EL LUGAR PARA IMPRMIR HACE UN INNER!!!! Mediante la funcion
-        //funcion para escribir
-        function escribirTarjeta(){tarjetasEnHTML.innerHTML +=  viajeEnTarjetaIndex};
-        //funcion SI con funcion de escribir, sino (sercarry.html) consolea eso. 
-        tarjetasEnHTML != null ? escribirTarjeta() : console.log("No se encuentra el id") ;
+                console.log(viajeEnTarjetaIndex);
+                //Codigo para subir a array y despues filtrar
+                arraysViajes.push(nuevoViaje);
+                localStorage.setItem(`viajes`, JSON.stringify(arraysViajes));
+            }
+            //Si no hay formulario (index.html) Consolea esto
+        });
+    }) : console.log("No se encuentra FormularioCarry");
+//FUNCION SI, SI EXISTE EL LUGAR PARA IMPRMIR HACE UN INNER!!!! Mediante la funcion
+//funcion para escribir
+function escribirTarjeta() { tarjetasEnHTML.innerHTML += viajeEnTarjetaIndex };
+//funcion SI con funcion de escribir, sino (sercarry.html) consolea eso. 
+tarjetasEnHTML != null ? escribirTarjeta() : console.log("No se encuentra el id");
+
+//____________________________________________//
+//Codigo para filtrar y generar tarjetas
+let infoBusqueda;
+let busquedaFiltro = document.getElementById("busquedaFiltro");
+let infoFiltroMayuscula;
+let infoFiltro;
+let cargadosArray;
+let filtrados;
+//FUNCION SI
+
+busquedaFiltro != null ?
+    //escucha evento Submit
+    busquedaFiltro.addEventListener("submit", (e) => {
+        e.preventDefault();
+        infoBusqueda = e.target.children;
+        //variable del form busqueda
+        infoFiltro = document.getElementById("infoFiltro").value;
+        infoFiltroMayuscula = infoFiltro.toUpperCase();
+        console.log (infoFiltroMayuscula);
+
+    //recupero el array de todos los viajes
+    cargadosArray = JSON.parse(localStorage.getItem(`viajes`));
+    console.log (cargadosArray);
+    //filtro los viajes de ese destino
+    filtrados = cargadosArray.filter(viaje => viaje.destino == infoFiltroMayuscula);
+    console.log(filtrados);
+        //POR CADA OBJETO FILTRADO CREO LA TARJETA
+        tarjetasJson
+
+
+
+
+
+        
+}) : "";
